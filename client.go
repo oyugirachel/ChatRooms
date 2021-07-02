5d8340774c21ecdf3a4c48791f4309f3a7b2b4c8
oyugirachel <oyugirachel20@gmail.com>
2021-07-02 11:26:48 +0300

    craeting a client to handle the requests

--- client.go
+++ client.go
@@ -32,3 +32,35 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
  }
+
+
+// Defining the connection type
+
+type connection struct {
+   ws *websocket.Conn
+   send chan []byte
+}
+
+// The readPump and writePump are the simple functions to pump the message from socket to hub and from hub to socket
+
+func (s subscription) readPump() {
+	c := s.conn
+	defer func() {
+	   Hub.unregister <- s
+	   c.ws.Close()
+	}()
+	c.ws.SetReadLimit(maxMessageSize)
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+	   _, msg, err := c.ws.ReadMessage()
+	   if err != nil {
+		  if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+			 log.Printf("error: %v", err)
+		  }
+		  break
+	   }
+	   m := message{msg, s.room}
+	   Hub.broadcast <- m
+	}
+ }
